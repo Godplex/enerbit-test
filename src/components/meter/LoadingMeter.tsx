f09efbf2f1b684fa09352caaf0e54e519a4beed8
Julian Rincon <julian1918@hotmail.es>
2023-01-20 16:48:23 -0500

    Change style loading

--- src/components/meter/LoadingMeter.tsx
+++ src/components/meter/LoadingMeter.tsx
@@ -2,10 +2,10 @@
 export const LoadingMeter = () => {
     return (
         <div className='d-flex flex-column justify-content-center align-items-center'>
-            <div className="spinner-border" role="status">
+            <div className="spinner-border text-danger" role="status">
                 <span className="visually-hidden">Loading...</span>
             </div>
-            <p>Cargando...</p>
+            <p>Loading...</p>
         </div>
     )
 }
